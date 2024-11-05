3971c56ecc40a2ea8699c17fdeec6e585c27d7dd
BUSI JOSEPH <94781818+busijoseph826@users.noreply.github.com>
2024-11-05 21:53:40 +0530

    creating employee class with some properties then access these properties in a method and we have to call the method in main method

--- OOPs/Inheritance/programs.java
+++ OOPs/Inheritance/programs.java
@@ -1,3 +1,6 @@
+
+//------------------------------------------------05-11-2024---------------------------------------------------------------------------
+//-------------------------------------------------Single Level Inheritance ---------------------------------------------------------------
 /* 1. create a java application where we have one class which contains non-static mthod m1() having param and return type, we have anther class which contains static method m2() having param and return type then invoke m1,m2 under main method by providing dynamic inputs*/
 
 import java.util.Scanner;
@@ -21,3 +24,26 @@ class Sample1 extends Sample{
 		System.out.println(m2(sc.next()));
 	}
 }
+/*2.create a java application where we have software company it will provide employee details like emp_id, emp_desigantin, salary, location to one employee then employee has to access these prperties and need to print these values with respcet to one method then invoke that method under main method by providing dynamic inputs.*/
+
+import java.util.Scanner;
+class Employee{
+	static Scanner sc = new Scanner(System.in);
+	int emp_id = sc.nextInt();
+	String designation  = sc.next();
+	double salary = sc.nextDouble();
+	String location = sc.next();
+	void display(){
+		System.out.println("Emp id : "+emp_id);
+		System.out.println("Emp Designation : "+designation);
+		System.out.println("Emp Salary : "+salary);
+		System.out.println("Emp Location : "+location);
+	}
+}
+class Employee1 extends Employee{
+	public static void main(String[]args){
+		new Employee1().display();
+	}
+}
+
+
